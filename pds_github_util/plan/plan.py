40f19c078411c66ded3fd3968a4feec6ea8551ca
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-10-13 08:35:06 -0700

    update backlog link

--- pds_github_util/plan/plan.py
+++ pds_github_util/plan/plan.py
@@ -23,7 +23,7 @@ REPO_INFO = ('\n--------\n\n'
              '   * - `User Guide <{}>`_\n'
              '     - `Github Repo <{}>`_\n'
              '     - `Issue Tracking <{}/issues>`_ \n'
-             '     - `Backlog <{}/issues>`_ \n'
+             '     - `Backlog <{}/issues?q=is%3Aopen+is%3Aissue+label%3Abacklog>`_ \n'
              '     - `Stable Release <{}/releases/latest>`_ \n'
              '     - `Dev Release <{}/releases>`_ \n\n')
 
