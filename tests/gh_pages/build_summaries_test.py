30f3a90fef0da90f9e401d11c8d9cd9861fe110b
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-12-11 17:08:57 -0500

    wip: start rst implementation, debug slow 404 check request
    add RST format for release summary
    
    upgrade version for rst summary generation
    
    correct command line parsing
    
    make unit test work

--- tests/gh_pages/build_summaries_test.py
+++ tests/gh_pages/build_summaries_test.py
@@ -1,13 +1,18 @@
-# import unittest
-# import os
-# from pds_github_util.gh_pages.build_summaries import build_summaries
+import unittest
+import os
+from pds_github_util.gh_pages.build_summaries import build_summaries
 
-# TOKEN = os.environ.get('GITHUB_TOKEN') or os.environ.get('ADMIN_GITHUB_TOKEN')
+TOKEN = os.environ.get('GITHUB_TOKEN')
 
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         build_summaries(TOKEN)
+class MyTestCase(unittest.TestCase):
+    def test_default_summaries(self):
+        #pass
+        build_summaries(TOKEN, path='tmp', format='md')
+
+    def test_rst_summaries(self):
+        #pass
+        build_summaries(TOKEN, path='tmp', format='rst')
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
