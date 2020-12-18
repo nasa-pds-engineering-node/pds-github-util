c83a158ebd36d2877c49c319eabf73cacb243d34
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-12-18 10:11:56 -0800

    comment out test that takes forever

--- tests/gh_pages/build_summaries_test.py
+++ tests/gh_pages/build_summaries_test.py
@@ -1,13 +1,13 @@
-import unittest
-import os
-from pds_github_util.gh_pages.build_summaries import build_summaries
+# import unittest
+# import os
+# from pds_github_util.gh_pages.build_summaries import build_summaries
 
-TOKEN = os.environ.get('GITHUB_TOKEN') or os.environ.get('ADMIN_GITHUB_TOKEN')
+# TOKEN = os.environ.get('GITHUB_TOKEN') or os.environ.get('ADMIN_GITHUB_TOKEN')
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        build_summaries(TOKEN)
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         build_summaries(TOKEN)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
