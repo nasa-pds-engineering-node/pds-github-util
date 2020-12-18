9bdec5b79e8612072454d8ce15d8b81770db58e7
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2020-12-18 09:39:57 -0800

    fix tests to use ADMIN_GITHUB_TOKEN

--- tests/gh_pages/build_summaries_test.py
+++ tests/gh_pages/build_summaries_test.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from pds_github_util.gh_pages.build_summaries import build_summaries
 
-TOKEN = os.environ.get('GITHUB_TOKEN')
+TOKEN = os.environ.get('GITHUB_TOKEN') or os.environ.get('ADMIN_GITHUB_TOKEN')
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
